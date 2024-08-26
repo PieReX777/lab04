e0f2e6c787f6b2b01d5b4a9d2c55a4702922b3ac
Jeanpiero Zea <jeanpiero.zea@tecsup.edu.pe>
2024-08-26 14:07:40 -0500

    Commit 3: Añadiendo Grid, Checkbox

--- app/src/main/java/com/example/androidlab04/MainActivity.kt
+++ app/src/main/java/com/example/androidlab04/MainActivity.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androidlab04.ui.theme.Androidlab04Theme
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Checkbox
 
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +35,11 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+
                     )
+                    MyLazyColumn()
+                    MyLazyRow()
+                    MyGrid()
                 }
             }
         }
@@ -65,7 +74,34 @@ fun MyLazyRow() {
         }
     }
 }
+@Composable
+fun MyGrid() {
+    val gridItems = List(9) { index -> "Item $index" }
+    LazyVerticalGrid(cells = GridCells.Fixed(3)) {
+        items(gridItems) { item ->
+            Card(
+                modifier = Modifier.padding(8.dp),
+            ) {
+                Checkbox(
+                    checked = false,
+                    onCheckedChange = { /* Handle checkbox state */ }
+                )
+            }
+        }
+    }
+}
 
+fun Checkbox(checked: Boolean, onCheckedChange: (Boolean) -> Unit, interactionSource: () -> Unit) {
+
+}
+
+fun items(gridItems: List<String>, function: @Composable() (LazyGridItemScope.(index: Int) -> Unit)) {
+    TODO("Not yet implemented")
+}
+
+fun LazyVerticalGrid(cells: Any, function: LazyGridScope.() -> Unit) {
+    TODO("Not yet implemented")
+}
 
 @Preview(showBackground = true)
 @Composable
